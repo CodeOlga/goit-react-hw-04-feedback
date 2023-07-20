dd6611aaae1e17b09f651d2b6d6d3d1350997339
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-07-20 22:33:25 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistic from './Statistics/Statistics';
@@ -6,41 +6,44 @@ import Notification from './Notification/Notification';
 import css from './App.module.css';
 
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onBtnClicked = option => {
+    switch (option) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        break;
+    }
   }
 
-  //робимо стрілку, щоб прив'язати this
-  //у стрілки контекст з'являється в момент створення
-  //у стрілки немає свого контекста, тому вона забирає його від батька
-  
-  onBtnClicked = option => 
-    this.setState(prevState => ({ [option]: prevState[option] + 1 }))
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   }
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     return total === 0 ? 0 : Math.round((good / total) * 100);
-  };
+  }
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    const options = Object.keys(this.state);
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+  const options = ['good', 'neutral', 'bad'];
 
     return (
       <div className={css.container}>
         <Section title="Please Leave feedback">
-          <FeedbackOptions options={options} onLeaveFeedback={this.onBtnClicked} />
+          <FeedbackOptions options={options} onLeaveFeedback={onBtnClicked} />
         </Section>
 
         <Section title="Statistics">
@@ -53,7 +56,9 @@ class App extends Component {
         </Section>
       </div>
     )
-  }
 }
 
-export default App;
+
+
+
+
